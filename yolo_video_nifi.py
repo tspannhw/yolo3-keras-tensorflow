3a4db1d19cf779f0d5cc53d2a693560bad94a58a
Timothy Spann <tspann@hortonworks.com>
2018-05-26 20:32:05 -0400

    Add files via upload

---
+++ yolo_video_nifi.py
@@ -0,0 +1,26 @@
+import time
+import sys
+import datetime
+import subprocess
+import sys
+import os
+import traceback
+import math
+import random, string
+import base64
+import uuid
+import json
+import psutil
+import random, string
+from time import gmtime, strftime
+import colorsys
+import os
+import random
+from yolonifi import YOLO
+from yolonifi import detect_video
+
+
+
+if __name__ == '__main__':
+    video_path='path2your-video'
+    detect_video(YOLO(), video_path)
